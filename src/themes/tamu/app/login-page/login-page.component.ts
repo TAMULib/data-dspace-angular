7c566d6097ac0c1c0f131c672c6407ed6e0a0f42
William Welling <wwelling@tamu.edu>
2025-06-18 14:25:22 -0500

    Upgrade tamu login page component

--- src/themes/tamu/app/login-page/login-page.component.ts
+++ src/themes/tamu/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { ThemedLogInComponent } from 'src/app/shared/log-in/themed-log-in.component';
+
 import { LoginPageComponent as BaseComponent } from '../../../../app/login-page/login-page.component';
 
 /**
@@ -8,8 +11,13 @@ import { LoginPageComponent as BaseComponent } from '../../../../app/login-page/
   selector: 'ds-login-page',
   // styleUrls: ['./login-page.component.scss'],
   styleUrls: ['../../../../app/login-page/login-page.component.scss'],
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
   // templateUrl: '../../../../app/login-page/login-page.component.html'
+  standalone: true,
+  imports: [
+    ThemedLogInComponent,
+    TranslateModule,
+  ],
 })
 export class LoginPageComponent extends BaseComponent {
 }
