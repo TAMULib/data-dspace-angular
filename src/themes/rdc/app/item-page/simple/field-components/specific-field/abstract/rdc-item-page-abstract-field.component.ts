15fd97c1331e32ebeb86121ba51e97d30aaf9445
William Welling <wwelling@tamu.edu>
2025-06-20 10:16:03 -0500

    Upgrade rdc theme item page abstract field component

--- src/themes/rdc/app/item-page/simple/field-components/specific-field/abstract/rdc-item-page-abstract-field.component.ts
+++ src/themes/rdc/app/item-page/simple/field-components/specific-field/abstract/rdc-item-page-abstract-field.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import {
+  Component,
+  Input,
+} from '@angular/core';
 
 import { ItemPageAbstractFieldComponent } from '../../../../../../../../app/item-page/simple/field-components/specific-field/abstract/item-page-abstract-field.component';
+import { MetadataValuesComponent } from '../../../../../../../../app/item-page/field-components/metadata-values/metadata-values.component';
 
 @Component({
   selector: 'rdc-ds-item-page-abstract-field',
-  templateUrl: '../../../../../../../../app/item-page/simple/field-components/specific-field/item-page-field.component.html'
+  templateUrl: '../../../../../../../../app/item-page/simple/field-components/specific-field/item-page-field.component.html',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    MetadataValuesComponent,
+  ],
 })
 export class RdcItemPageAbstractFieldComponent extends ItemPageAbstractFieldComponent {
 
