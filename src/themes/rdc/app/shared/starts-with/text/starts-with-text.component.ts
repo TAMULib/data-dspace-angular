3273eb9992293a307ebb43401747a02bd853f3b4
William Welling <wwelling@tamu.edu>
2025-06-25 13:32:43 -0500

    Update rdc starts with text component

--- src/themes/rdc/app/shared/starts-with/text/starts-with-text.component.ts
+++ src/themes/rdc/app/shared/starts-with/text/starts-with-text.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
-import { renderStartsWithFor, StartsWithType } from '../../../../../../app/shared/starts-with/starts-with-decorator';
 import {
-  StartsWithTextComponent as BaseComponent
-} from '../../../../../../app/shared/starts-with/text/starts-with-text.component';
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+// TAMU Customizations
+import { RouterLink } from '@angular/router';
+// END TAMU Customizations
+import { TranslateModule } from '@ngx-translate/core';
+
+import { StartsWithTextComponent as BaseComponent } from '../../../../../../app/shared/starts-with/text/starts-with-text.component';
+import { StartsWithType } from '../../../../../../app/shared/starts-with/starts-with-type';
+import { renderStartsWithFor } from '../../../../../../app/shared/starts-with/starts-with-decorator';
+
 
 @Component({
   selector: 'ds-starts-with-text',
@@ -10,6 +19,15 @@ import {
   styleUrls: ['../../../../../../app/shared/starts-with/text/starts-with-text.component.scss'],
   templateUrl: './starts-with-text.component.html',
   // templateUrl: '../../../../../../app/shared/starts-with/text/starts-with-text.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    // TAMU Customizations
+    RouterLink,
+    // END TAMU Customizations
+    TranslateModule,
+  ],
 })
 @renderStartsWithFor(StartsWithType.text)
 export class StartsWithTextComponent extends BaseComponent {
